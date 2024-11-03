726f80ee71d91bd3c4d2a11f3b68b0e1a1d29447
AnshuGupta00 <anshu.gupta351613@gmail.com>
2024-11-03 17:14:51 +0530

    new

--- Password/src/App.jsx
+++ Password/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setlength] = useState(0)
+  const [Numallowed, setnumallowed] = useState(0)
+  const[charallowed, setcharallowed] =useState(false)
+  const [password, setpassword] = useState(false)
+
+const PasswordGenrator=()=>{}
 
   return (
     <>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-black justify-center text-3xl'
+       ><h1 className='text-white text-center my-3'>Passwor Genrator</h1>
+     <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input type="text" 
+        placeholder='Password'
+        readOnly
+         className="outline-none w-full py-1 px-3"
+      />
+      <button className='bg-orange-400 text-2xl text-center rounded-md' >Copy</button>
+     </div>
+     <div className='flex text-sm gap-x-2'>
+     <input type="range" 
+     min={8}
+     max={25}
+     className="cursor-pointer"
+     />
+      </div>
+        <div className="flex items-center gap-x-1">
+          <input type="checkbox" id="Numbers" />
+        <label htmlFor="Numbers" className='text-base text-white'> Numbers </label>
+        </div>
+        
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </div>
     </>
   )
 }
