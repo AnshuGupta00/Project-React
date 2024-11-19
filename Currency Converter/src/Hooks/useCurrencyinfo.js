829901e9bdc6578434649864df7a6735c560aeea
AnshuGupta00 <anshu.gupta351613@gmail.com>
2024-11-19 18:57:38 +0530

    dont'konw

---
+++ Currency Converter/src/Hooks/useCurrencyinfo.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react"
+
+
+  
+const [data, serData]=useState({})
+function useCurrencyinfo(currency){
+    useEffect(()=>{
+        fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json')
+        .then((res) => res.json())
+        .then((res) => serData(res[currency]))
+        console.log(data);
+    },[currency])
+    console.log(data);
+    return data;
+}
+    
+export default useCurrencyinfo;
